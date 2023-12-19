12c03fc494019653633464537a3131959920e8dc
Raj0972 <rajashekark0972@gmail.com>
2023-12-19 16:37:06 +0530

    intrest

---
+++ task.py
@@ -0,0 +1,9 @@
+p,r,t = input("Enter values = ").split()
+
+print("Principle Amount is :",p)
+print("Rate of intrest : ",r)
+print("Time Period in months : ",t)
+
+x = (int(p)*int(r)*int(t)) / 100
+
+print("Simple Intrest is ",x)
